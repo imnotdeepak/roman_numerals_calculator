92519820b2e11014ffb4c81caf000b196e2058c7
Deepak <65418771+imnotdeepak@users.noreply.github.com>
2024-01-17 12:41:35 -0500

    Add files via upload

---
+++ app.py
@@ -0,0 +1,15 @@
+def romanNums(x):
+    numbers = [1, 4, 5, 9, 10, 40, 50, 90, 100, 400, 500, 900, 1000]
+    roman = ['I', 'IV', 'V', 'IX', 'X', 'XL', 'L', 'XC', 'C', 'CD', 'D', 'CM', 'M']
+    i = 12
+    roman_numeral = ''
+    while x != 0:
+        if numbers[i] <= x:
+            roman_numeral += roman[i]
+            x = x - numbers[i]
+        else:
+            i -= 1
+    return roman_numeral
+
+print(romanNums())
+
